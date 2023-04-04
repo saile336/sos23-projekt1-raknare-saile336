e791640ffa6f650bde46112d33ca1d7161dcadf1
Saile336 <elias.nybondas@gmail.com>
2023-04-04 14:26:52 +0300

    commit

--- app/src/main/java/fi/arcada/sos22_1_raknare/MainActivity.java
+++ app/src/main/java/fi/arcada/sos22_1_raknare/MainActivity.java
@@ -3,12 +3,30 @@ package fi.arcada.sos22_1_raknare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView outputText;
+    EditText inputText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Koppling mellan XML layout och koden
+        outputText = findViewById(R.id.outputText);
+        inputText = findViewById(R.id.editTextName);
+
+        outputText.setText("-");
     }
+
+    public void buttonHandler(View view) {
+        //Avläser text fältet och omvandlar editable objektet till en string
+        String namn = inputText.getText().toString();
+        outputText.setText("Hej " + namn);
+    }
+
 }
